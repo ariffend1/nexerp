8ce64fef16b7d04db1a7e23fa78b4b0eb3744a2b
ariffend1 <faeyza.solusi@gmail.com>
2025-12-22 13:33:39 +0700

    Fix SQLAlchemy import and Dockerfile permissions

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,8 +1,8 @@
 import os
-from sqlalchemy import create_url
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 from dotenv import load_dotenv
 
 load_dotenv()
